6a5fd281b4853cc7d374bd20246d044ad55b769a
Your Name <you@example.com>
2023-10-22 14:31:11 +0600

    set up .env.local file with firebase.config.js file

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAjUEBxQU4i6Hx1FmDIFCIllyxoaWShpvk",
-    authDomain: "dental-consultation-cd8c2.firebaseapp.com",
-    projectId: "dental-consultation-cd8c2",
-    storageBucket: "dental-consultation-cd8c2.appspot.com",
-    messagingSenderId: "706408951781",
-    appId: "1:706408951781:web:81bc22c94a17114b0dc90c"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
