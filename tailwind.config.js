ceb9e66e1752f03813dc541897d26040a2259bc3
Your Name <you@example.com>
2023-10-25 19:22:08 +0600

    Create simple Top Banner Using Hero component

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,32 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  daisyui: {
+    themes: [
+      {
+        dentaltheme: {
+
+          primary: "#0FCFEC",
+
+          secondary: "#19D3AE",
+
+          accent: "#3A4256",
+
+          neutral: "#2b3440",
+
+          "base-100": "#ffffff",
+
+          info: "#3abff8",
+
+          success: "#36d399",
+
+          warning: "#fbbd23",
+
+          error: "#f87272",
+        },
+      },
+    ],
+  },
   theme: {
     extend: {},
   },
