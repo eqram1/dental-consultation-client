b147b44a05fc1a7bd15470fa0d343d61c1f08573
Your Name <you@example.com>
2023-11-01 22:33:42 +0600

    updated available appointments components

--- src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
@@ -12,9 +12,9 @@ const AvailableAppointments = ({ selectedDate }) => {
             .then(data => setAppointmentOptions(data))
     }, [])
     return (
-        <section className='my-16 text-center text-primary font-bold text-xl'>
-            <p>Available Appointments on April {format(selectedDate, 'PP')}</p>
-            <div className='grid gap-6 my-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+        <section className='my-16'>
+            <p className='text-center text-secondary font-bold'>Available Appointments on {format(selectedDate, 'PP')}</p>
+            <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-6'>
                 {
                     appointmentOptions.map(option => <AppointmentOption
                         key={option._id}
